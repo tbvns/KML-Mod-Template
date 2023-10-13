21b2bdf6f2758b0ed7c06357c628ab1b58530d04
tbvns <69420062+tbvns25565@users.noreply.github.com>
2023-10-13 19:30:38 +0000

    Updated the mod to keep up with API changes.

--- src/main/java/xyz/tbvns/Main.java
+++ src/main/java/xyz/tbvns/Main.java
@@ -1,12 +1,19 @@
 package xyz.tbvns;
 
 import xyz.prismenetwork.kelpmodloader.ModsAPI.Mods;
+import xyz.prismenetwork.kelpmodloader.Pack.CreatePack;
+import xyz.prismenetwork.kelpmodloader.Texture.TextureType;
+
+import java.io.InputStream;
+import java.net.URISyntaxException;
 
 
 public class Main {
-    public static Mods load(Mods mods) {
+    public Mods load(Mods mods) throws URISyntaxException {
         System.out.println("Hello world from KelpModLoaders API !");
-        mods.addBlock("Test", "stone");
+        mods.addBlock("Test", "yay2");
+        System.out.println(this.getClass().getClassLoader().getResource("textures/yay2.png").toURI());
+        mods.addTexture("yay2", TextureType.Block(), "textures/ya y2.png");
 
         return mods;
     }
