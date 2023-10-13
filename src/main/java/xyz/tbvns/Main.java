04b35b0605508ed70866c04f441649ead4aa986d
tbvns <69420062+tbvns25565@users.noreply.github.com>
2023-10-13 19:31:20 +0000

    Removed some test code.

--- src/main/java/xyz/tbvns/Main.java
+++ src/main/java/xyz/tbvns/Main.java
@@ -12,7 +12,6 @@ public class Main {
     public Mods load(Mods mods) throws URISyntaxException {
         System.out.println("Hello world from KelpModLoaders API !");
         mods.addBlock("Test", "yay2");
-        System.out.println(this.getClass().getClassLoader().getResource("textures/yay2.png").toURI());
         mods.addTexture("yay2", TextureType.Block(), "textures/ya y2.png");
 
         return mods;
--- src/main/java/xyz/tbvns/TestClass.java
+++
@@ -1,7 +0,0 @@
-package xyz.tbvns;
-
-public class TestClass {
-    public void PrintTest() {
-        System.out.println("Hello world from another class");
-    }
-}
